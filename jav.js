d75cdd4942d30d35c4847fdc315e9381b83e6ed9
jordan2025clemente <jordanclemente2025@gmail.com>
2025-03-10 13:53:27 +0800

    Add files via upload

---
+++ jav.js
@@ -0,0 +1,9 @@
+
+let visits = localStorage.getItem("visits");
+if (!visits) {
+    visits = 0;
+} else {
+    visits = parseInt(visits) + 1;
+}
+localStorage.setItem("visits", visits);
+document.getElementById("counter").textContent = visits;
